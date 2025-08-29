278fcfc8f11fd7555f9c5e308f3925d32cc74b7d
Carlsonio <kirill.shevtsov2006@gmail.com>
2025-08-29 15:51:49 +0700

    Изменил AndroidManifest.xml

---
+++ app/src/main/java/com/example/hold/MainApplication.kt
@@ -0,0 +1,12 @@
+package com.example.hold
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // You can add any application-specific initialization here
+    }
+}
